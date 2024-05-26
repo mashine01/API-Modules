86fddcff4503e90e8fd0b48c60f7f3cfb5b76f97
mashine01 <mubeensyed01@hotmail.com>
2024-05-26 15:01:54 +0500

    Update main.py

--- main.py
+++ main.py
@@ -25,5 +25,4 @@ ngrok.set_auth_token(auth_token)
 ngrok_tunnel = ngrok.connect(8000)
 print('Public URL:', ngrok_tunnel.public_url)
 
-nest_asyncio.apply()
 uvicorn.run(app, host="0.0.0.0", port=8000)
