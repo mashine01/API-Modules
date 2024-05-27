75fc9df74e0ea34f55c9a8952913d57f525e3061
mashine01 <mubeensyed01@hotmail.com>
2024-05-28 02:41:41 +0500

    Update llama_model.py

--- llama_model.py
+++ llama_model.py
@@ -1,15 +1,14 @@
 from huggingface_hub import hf_hub_download
-from langchain_community.llms import LlamaCpp
-from langchain_core.callbacks import CallbackManager, StreamingStdOutCallbackHandler
-from langchain_core.prompts import PromptTemplate
+from llama_cpp import Llama
 from config import MODEL, MODEL_BASENAME
 
 model_path = hf_hub_download(repo_id=MODEL, filename=MODEL_BASENAME)
 
-lcpp_llm = LlamaCpp(
+lcpp_llm = Llama(
     model_path=model_path,
     n_threads=4,
     n_batch=64,
     n_ctx=4096,
-    n_gpu_layers=32,
+    n_gpu_layers=64,
+    f16_kv=True
 )
