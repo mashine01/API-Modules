800d74251f00a5f663fc8e9e4b4829c4fd532f6a
mashine01 <mubeensyed01@hotmail.com>
2024-05-28 19:17:04 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ sudo apt upgrade
 sudo apt install python3
 sudo apt install python3-pip
 pip install FastAPI nest-asyncio pyngrok uvicorn python-multipart huggingface_hub numpy nest_asyncio --quiet
-CMAKE_ARGS="-DLLAMA_CUDA=on" FORCE_CMAKE=1 pip install llama-cpp-python --quiet
+CMAKE_ARGS="-DLLAMA_CUBLAS=on" FORCE_CMAKE=1 pip install llama-cpp-python --quiet
